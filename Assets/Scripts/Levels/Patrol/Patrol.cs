539c575e9d8eb9622fd828cfea023b9b0b121827
Michail Eshmakov <mihail.eshmakov@yandex.ru>
2021-07-15 11:13:35 +0500

    Создан уровень с джоинтами #22

---
+++ Assets/Scripts/Levels/Patrol/Patrol.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Patrol : MonoBehaviour
+{
+    [SerializeField] private MovePoint[] _movePoints;
+    [SerializeField] private float _speed;
+
+    private int _targetPointIndex = 0;
+
+    protected MovePoint TargetPoint => _movePoints[_targetPointIndex];
+    protected float Speed => _speed;
+
+    protected void SetNextTargetPoint()
+    {
+        if (_targetPointIndex < _movePoints.Length - 1)
+            _targetPointIndex++;
+        else
+            _targetPointIndex = 0;
+    }
+}
--- Assets/Scripts/PlayerPlacer.cs
+++ Assets/Scripts/PlayerPlacer.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class StartPlayerPlacer : Singleton<StartPlayerPlacer>
+public class PlayerPlacer : Singleton<PlayerPlacer>
 {
     private Vector3 _startPosition;
+    private LevelBorder _levelBorder;
 
     public static event UnityAction PlayerPlaced;
 
     protected override void Awake()
     {
-        _startPosition = FindObjectOfType<StartPosition>().transform.position;
+        //_startPosition = FindObjectOfType<StartPosition>().transform.position;
+        //_levelBorder = FindObjectOfType<LevelBorder>();
         SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneManager.sceneUnloaded += OnSceneUnloaded; ;
         base.Awake();
         PlacePlayer();
-
     }
 
     private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
+        _levelBorder = FindObjectOfType<LevelBorder>();
+        if (_levelBorder != null)
+            _levelBorder.PlayerOuted += OnPlayerOuted;
+
         _startPosition = FindObjectOfType<StartPosition>().transform.position;
+        PlacePlayer();
+    }
+
+    private void OnSceneUnloaded(Scene arg0)
+    {
+        if (_levelBorder != null)
+        {
+            _levelBorder.PlayerOuted -= OnPlayerOuted;
+            _levelBorder = null;
+        }
+    }
+
+    private void OnPlayerOuted()
+    {
         PlacePlayer();
     }
 
