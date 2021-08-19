5e0ee5adcaaee3170b79a9ab3619f55eb91b2db8
Michail Eshmakov <mihail.eshmakov@yandex.ru>
2021-08-19 19:11:45 +0500

    Настроен достeп к кнопке открывания меню эффектов #54

--- Assets/Scripts/UI/QuestionsPanel.cs
+++ Assets/Scripts/UI/QuestionsPanel.cs
@@ -2,20 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Button))]
 public class QuestionsPanel : MonoBehaviour
 {
     [SerializeField] TMP_Text _questionsText;
     [SerializeField] QuestionScore _questionScore;
+    [SerializeField] TestModeSetter _testModeSetter;
+    [SerializeField] GameObject _effectsPanel;
 
     private bool _isSubscribedOnQuestionsChanged = false;
+    private Button _button;
 
     public event UnityAction SubscribedOnQuestionsChanged;
     public bool IsSubscribedOnQuestionsChanged => _isSubscribedOnQuestionsChanged;
 
-    private void Awake()
+    private void Start()
     {
+        _button = GetComponent<Button>();
+        if (_testModeSetter.IsTestMode)
+            _button.onClick.AddListener(OnClick);
+        else
+            _button.interactable = false;
+
         _questionScore.QuestionsChanged += OnQuestionsChanged;
         SubscribedOnQuestionsChanged?.Invoke();
         _isSubscribedOnQuestionsChanged = true;
@@ -23,8 +34,13 @@ public class QuestionsPanel : MonoBehaviour
 
     private void OnDestroy()
     {
+        _button.onClick.RemoveListener(OnClick);
         if (_questionScore != null)
             _questionScore.QuestionsChanged -= OnQuestionsChanged;
+    }
+    private void OnClick()
+    {
+        _effectsPanel.SetActive(true);
     }
 
     public void OnQuestionsChanged(int questions)
